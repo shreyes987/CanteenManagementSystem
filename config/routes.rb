e605a78a4cd1878031c84dcefd51529a327c48f7
shreyes987 <shreyes987@gmail.com>
2020-11-21 16:40:31 +0530

    adding order_item and order

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+
   scope :api, defaults: { format: :json } do
-    devise_for :users,controllers: { sessions: :sessions ,registrations: :registrations},
+    devise_for :users, controllers: { sessions: :sessions  , registrations: :registrations},
                path_names: { sign_in: :login }
+    resource :order_items, only: [:create]
+    resource :menus, only: [:show , :create]
+    post 'orders/update_order_status'
+    post 'orders/cancel_order'
+    post 'orders/update_payment_status'
   end
-  post 'orders/update_order_status'
-  post 'orders/update_payment_status'
-  post 'orders/cancel_order'
-  resource :order_items, only: [:show , :create]
   resource :user, only: [:show, :update]
-  resource :menus, only: [:show , :create]
 end
